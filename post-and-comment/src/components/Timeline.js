5c8204c14c8f1ce23b59b8caffb5720c0a1df7a2
britof <41692419+britof@users.noreply.github.com>
2020-05-02 11:39:08 -0300

    Post & Comment
    Simple post&comment front-end made with ReactJS

---
+++ post-and-comment/src/components/Timeline.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class Timeline extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            posts: [],
+            postTitle: '',
+            postText: '',
+            postComments
+        }
+    }
+
+    render() {
+
+    }
+}
